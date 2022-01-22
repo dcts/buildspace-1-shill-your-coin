6e9f0161de7aad52a964d50864cb294a83e32afc
dcts <thomas@liist.com>
2022-01-22 22:39:30 +0100

    :bug: fix typo

--- scripts/run.js
+++ scripts/run.js
@@ -1,5 +1,5 @@
 const main = async () => {
-  const shillContractFactory = await hre.ethers.getContractFactor("ShillPortal");
+  const shillContractFactory = await hre.ethers.getContractFactory("ShillPortal");
   const shillContract = await shillContractFactory.deploy();
   await shillContract.deployed();
   console.log("Contract deployed to:", shillContract.address);
