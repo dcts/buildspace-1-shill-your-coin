9b9a109184528131c3a3c704733ad9da165216f6
dcts <thomas@liist.com>
2022-01-22 22:39:09 +0100

    :sparkle: add script

---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+  const shillContractFactory = await hre.ethers.getContractFactor("ShillPortal");
+  const shillContract = await shillContractFactory.deploy();
+  await shillContract.deployed();
+  console.log("Contract deployed to:", shillContract.address);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch(error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+runMain();
