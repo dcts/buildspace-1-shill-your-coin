ddeed8a37e23f21b937492a75c8c61618b7d2f19
dcts <thomas@liist.com>
2022-01-22 22:17:46 +0100

    :construction: save progress

---
+++ contracts/ShillPortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract ShillPortal {
+    constructor() {
+        console.log("This contract allows you to shill your favorite (shit)coin");
+    }
+}
