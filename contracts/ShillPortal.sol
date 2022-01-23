9ad922ea39b1d7f9ff7eff8bfb975e2da798f3d4
dcts <thomas@liist.com>
2022-01-23 03:13:11 +0100

    :contruction: save progress

--- contracts/ShillPortal.sol
+++ contracts/ShillPortal.sol
@@ -4,7 +4,36 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract ShillPortal {
-    constructor() {
-        console.log("This contract allows you to shill your favorite (shit)coin");
-    }
+  uint256 totalShills;
+  mapping(address => Shill) shillDict;
+
+  struct Shill {
+	  address sender;
+    string name;
+	  string coin;
+	}
+
+  function addShill(string memory _name, string memory _coin) public {
+    shillDict[msg.sender] = Shill(msg.sender, _name, _coin);
+    totalShills += 1;
+    console.log("%s (%s) shilled %s", msg.sender, _name, _coin);
+  }
+
+  function getShill(address targetWallet) public view returns(Shill memory) {
+    return shillDict[targetWallet];
+  }
+
+  constructor() {
+    console.log("This contract allows you to shill your favorite (shit)coin");
+  }
+
+  // function shill() public {
+  //   totalShills += 1;
+  //   console.log("%s has shilled a coin", msg.sender);
+  // }
+
+  function getTotalShills() public view returns (uint256) {
+    console.log("We have %d total coins shilled!", totalShills);
+    return totalShills;
+  }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,24 @@
 const main = async () => {
+  const [owner, randomPerson, randomPerson2] = await hre.ethers.getSigners();
   const shillContractFactory = await hre.ethers.getContractFactory("ShillPortal");
   const shillContract = await shillContractFactory.deploy();
   await shillContract.deployed();
+
   console.log("Contract deployed to:", shillContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await shillContract.getTotalShills();
+
+  let txn;
+  txn = await shillContract.addShill("dcts", "holotoken");
+  await txn.wait();
+  txn = await shillContract.connect(randomPerson).addShill("mersad", "olympus-dao");
+  await txn.wait();
+  txn = await shillContract.connect(randomPerson2).addShill("david", "ethereum");
+  await txn.wait();
+  txn = await shillContract.connect(randomPerson2).addShill("david", "bitcoin");
+
+  await shillContract.getTotalShills();
 }
 
 const runMain = async () => {
